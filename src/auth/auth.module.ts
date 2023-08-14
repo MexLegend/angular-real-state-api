e570c84495ebc56fe8663a424592da8745ac248e
MexLegend <armandoskate2011@hotmail.com>
2023-08-14 11:32:36 -0600

    Unused Functions Removed

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,7 +6,6 @@ import { AuthService } from './auth.service';
 import { AuthGuardProvider } from './guards/jwt-auth.guard';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
-import { GoogleStrategy } from './strategies/google.strategy';
 import { SessionSerializer } from './utils/serializer';
 import { UserService } from '../user/user.service';
 
@@ -15,7 +14,6 @@ import { UserService } from '../user/user.service';
   providers: [
     AuthGuardProvider,
     AuthService,
-    GoogleStrategy,
     JwtRefreshStrategy,
     JwtService,
     JwtStrategy,
